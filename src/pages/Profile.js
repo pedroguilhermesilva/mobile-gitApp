7bb6ee752126d5108d11840960a90b368e87bcd0
pedroguilhermesilva <pedroguuilherme@gmail.com>
2020-01-17 18:22:38 -0300

    finished front-end with intregation with back

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { View } from 'react-native';
 import { WebView } from 'react-native-webview';
 
-function Profile () {
-  return <WebView style={{ flex: 1 }} source={{ uri: 'https://github.com/pedroguilhermesilva'}}/>
+function Profile ({ navigation }) {
+  const githubUserName = navigation.getParam('github_username');
+  
+  return <WebView style={{ flex: 1 }} source={{ uri: `https://github.com/${githubUserName}`}}/>
 }
 
 export default Profile;
